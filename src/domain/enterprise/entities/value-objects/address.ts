9b6aa1a276a0305a8a2ea31a64358a875e731817
André Parada <andregparada@gmail.com>
2025-11-14 18:09:20 -0300

    feat: error handler either left or right

--- src/domain/enterprise/entities/value-objects/address.ts
+++ src/domain/enterprise/entities/value-objects/address.ts
@@ -1,24 +1,10 @@
 export class Address {
-  public neighborhood: string
-  public street: string
-  public number: string
-  public cep: string
-  public complement?: string
-  public additionalInfo?: string
-
   constructor(
-    neighborhood: string,
-    street: string,
-    number: string,
-    cep: string,
-    complement?: string,
-    additionalInfo?: string,
-  ) {
-    this.neighborhood = neighborhood
-    this.street = street
-    this.number = number
-    this.cep = cep
-    this.complement = complement
-    this.additionalInfo = additionalInfo
-  }
+    public readonly neighborhood: string,
+    public readonly street: string,
+    public readonly number: string,
+    public readonly cep: string,
+    public readonly complement?: string,
+    public readonly additionalInfo?: string,
+  ) {}
 }
